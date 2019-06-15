b34372e47cce7dce7dccb711bb870996b613ee63
Sapir Malka <itssapir@gmail.com>
2019-06-15 15:35:22 +0300

    test1 passed, 2 fails

--- core_api.cpp
+++ core_api.cpp
@@ -29,14 +29,15 @@ private:
     unsigned _idleUntil;
     tcontext _context;
     unsigned _currentInst;
-	unsigned _Tid;
+    unsigned _Tid;
+    bool _waitingLoad;
 };
 
 
 class CPU
 {
 public:
-    CPU(MODE mode) : _mode(mode), _clock(0) { _switchPenalty = (mode == FINEGRAIN)? 0:Get_switch_cycles(); }
+    CPU(MODE mode) : _mode(mode), _clock(0) {}
     void simReset(unsigned numThreads);
     void run(); // TODO: should we return anything?
     double getCPI();
@@ -63,9 +64,11 @@ void CPU::run()
         _liveThreads.pop_front(); // after iteration we will push it to the back if not done
         unsigned clocksRan = _threads[curThread].run(_clock); // run the thread until it goes to idle or 1 clock if we are in finegrain
         assert(clocksRan > 0);
-        _clock += _switchPenalty + clocksRan;
+        _clock += clocksRan;
         if(_threads[curThread].done == false)
             _liveThreads.push_back(curThread);
+        if(!_liveThreads.empty() )
+            _clock += _switchPenalty; // need to switch to another thread.
     }
 }
 
@@ -77,17 +80,18 @@ void CPU::simReset(unsigned numThreads)
     _numThreads = numThreads;
     _clock = 0;
     // create new thread objects
-    for (int i = 0; i < numThreads; ++i)
+    for (unsigned i = 0; i < numThreads; ++i)
     {
         _threads.push_back(Thread(_mode, i));
         _liveThreads.push_back(i);
     }
+     _switchPenalty = (_mode == FINEGRAIN)? 0:Get_switch_cycles();
 }
 
 double CPU::getCPI()
 {
     unsigned totalInsts = 0;
-    for(int i=0; i < _numThreads; ++i)
+    for(unsigned i=0; i < _numThreads; ++i)
         totalInsts += _threads[i].getTotalInstructions();
     return ( (double)_clock ) / totalInsts;
 }
@@ -98,7 +102,7 @@ void CPU::getThreadContext(tcontext* bcontext, int threadid)
 }
 
 Thread::Thread(MODE mode, unsigned Tid) : 
-	done(false), _mode(mode), _idleUntil(0), _currentInst(0), _Tid(Tid)
+	done(false), _mode(mode), _idleUntil(0), _currentInst(0), _Tid(Tid), _waitingLoad(false)
 {
 	//set the regs to 0
 	for (int i = 0; i < REGS; ++i) {
@@ -112,45 +116,54 @@ Thread::Thread(MODE mode, unsigned Tid) :
 }
 
 unsigned Thread::run(unsigned curClock) {
-	//if the thread is in idle state
-	int curRun = 0;
-	if (curClock < _idleUntil) {
-		return ++curRun;
-	}
-	//do the intructions
-	while (true) {
-		Instuction Inst;
-		SIM_MemInstRead(_currentInst, &Inst, _Tid);
-		switch (Inst.opcode) {
-			case CMD_NOP:
-				++curRun;
-				_currentInst++;
-				break;
-			case CMD_ADD:  // dst <- src1 + src2
-			case CMD_SUB:  // dst <- src1 - src2
-			case CMD_ADDI: // dst <- src1 + imm
-			case CMD_SUBI: // dst <- src1 - imm
-				arith_command(&Inst);
-				++curRun;
-				_currentInst++;
-				break;
-			case CMD_LOAD:   // dst <- Mem[src1 + src2]  (src2 may be an immediate)
-			case CMD_STORE:  // Mem[dst + src2] <- src1  (src2 may be an immediate) 
-				mem_command(&Inst, curRun + curClock);
-				++curRun;
-				_currentInst++;
-				break;
-			case CMD_HALT: //the end of these thread
-				done = true;
-				_currentInst++;
-				return ++curRun;
-		}
-		//if it's FINEGRAIN mode or the thread is in idle state 
-		if (_mode == FINEGRAIN || ((curRun + curClock) < _idleUntil)) {
-			break;
-		}
-	}
-	return curRun;
+    //if the thread is in idle state
+    int curRun = 0;
+    if (_idleUntil != 0 && curClock < _idleUntil) {
+            return ++curRun;
+    }
+    //do the intructions
+    while (true) {
+        Instuction Inst;
+        bool needSwitch = false;
+        if (_waitingLoad) {
+            _waitingLoad = false;
+            ++curRun;
+        }
+        else
+        {
+            SIM_MemInstRead(_currentInst, &Inst, _Tid);
+            switch (Inst.opcode) {
+                case CMD_NOP:
+                    ++curRun;
+                    _currentInst++;
+                    break;
+                case CMD_ADD:  // dst <- src1 + src2
+                case CMD_SUB:  // dst <- src1 - src2
+                case CMD_ADDI: // dst <- src1 + imm
+                case CMD_SUBI: // dst <- src1 - imm
+                    ++curRun;
+                    arith_command(&Inst);
+                    _currentInst++;
+                    break;
+                case CMD_LOAD:   // dst <- Mem[src1 + src2]  (src2 may be an immediate)
+                case CMD_STORE:  // Mem[dst + src2] <- src1  (src2 may be an immediate) 
+                    ++curRun;
+                    mem_command(&Inst, curRun + curClock);
+                    _currentInst++;
+                    needSwitch = true;
+                    break;
+                case CMD_HALT: //the end of these thread
+                    done = true;
+                    _currentInst++;
+                    return ++curRun;
+            }
+        }
+        //if it's FINEGRAIN mode or the thread is in idle state 
+        if (_mode == FINEGRAIN || needSwitch ) {
+            break;
+        }
+    }
+    return curRun;
 }
 
 void Thread::arith_command(Instuction* Inst) {
@@ -176,27 +189,28 @@ void Thread::arith_command(Instuction* Inst) {
 }
 
 void Thread::mem_command(Instuction* Inst, unsigned time) {
-	cmd_opcode opcode = Inst->opcode;
-	int dst_index = Inst->dst_index;
-	int src1_index = Inst->src1_index;
-	int src2_index_imm = Inst->src2_index_imm;
-	bool isSrc2Imm = Inst->isSrc2Imm;
+    cmd_opcode opcode = Inst->opcode;
+    int dst_index = Inst->dst_index;
+    int src1_index = Inst->src1_index;
+    int src2_index_imm = Inst->src2_index_imm;
+    bool isSrc2Imm = Inst->isSrc2Imm;
     uint32_t addr;
-	switch (opcode) {
-		case CMD_LOAD:   // dst <- Mem[src1 + src2]  (src2 may be an immediate)
-			 addr = (isSrc2Imm == true) ? _context.reg[src1_index] + src2_index_imm : 
-												  _context.reg[src1_index] + _context.reg[src2_index_imm];
-			SIM_MemDataRead(addr, _context.reg + dst_index);
-			_idleUntil += _latency.Load_latecny;
-			break;
-		case CMD_STORE:  // Mem[dst + src2] <- src1  (src2 may be an immediate) 		_context.reg[dst_index] = _context.reg[src1_index] - src2_index_imm;
-			addr = (isSrc2Imm == true) ? _context.reg[dst_index] + src2_index_imm :
-												  _context.reg[dst_index] + _context.reg[src2_index_imm];
-			SIM_MemDataWrite(addr, _context.reg[src1_index]);
-			_idleUntil += _latency.Store_latency;
-			break;
-	}
-	return;
+    switch (opcode) {
+        case CMD_LOAD:   // dst <- Mem[src1 + src2]  (src2 may be an immediate)
+            addr = (isSrc2Imm == true) ? _context.reg[src1_index] + src2_index_imm :
+                                          _context.reg[src1_index] + _context.reg[src2_index_imm];
+            SIM_MemDataRead(addr, _context.reg + dst_index);
+            _idleUntil = time + _latency.Load_latecny;
+            _waitingLoad =  true;
+            break;
+        case CMD_STORE:  // Mem[dst + src2] <- src1  (src2 may be an immediate) 		_context.reg[dst_index] = _context.reg[src1_index] - src2_index_imm;
+            addr = (isSrc2Imm == true) ? _context.reg[dst_index] + src2_index_imm :
+                                      _context.reg[dst_index] + _context.reg[src2_index_imm];
+            SIM_MemDataWrite(addr, _context.reg[src1_index]);
+            _idleUntil = time + _latency.Store_latency;
+            break;
+    }
+    return;
 }
 
 unsigned Thread::getTotalInstructions()
--- main.c
+++ main.c
@@ -11,8 +11,8 @@ int main(int argc, char const *argv[]){
 	        exit(2);
 	    }
 	int threads = Get_thread_number();
-	tcontext blocked[threads];
-	tcontext finegrained[threads];
+	tcontext blocked[4];
+	tcontext finegrained[4];
 	for(int k=0; k < Get_thread_number(); k++){
 	    for (int i = 0; i < REGS; ++i){
 	        finegrained[k].reg[i]=0;
@@ -22,7 +22,7 @@ int main(int argc, char const *argv[]){
 	Core_blocked_Multithreading();
 	printf("\n----Blocked Simulation -------\n");
 	for(int k=0; k < Get_thread_number(); k++){
-		Core_blocked_context(blocked,k);
+		Core_blocked_context(blocked+k,k);
 	    printf("\nRegister file thread id %d:\n",k);
 	    for (int i = 0; i < REGS; ++i)
 	        printf("\tR%d = 0x%X", i, blocked[k].reg[i]);
@@ -32,7 +32,7 @@ int main(int argc, char const *argv[]){
 	Core_fineGrained_Multithreading();
 	printf("\n-----Finegrained Simulation -----\n");
 	for(int k=0; k < Get_thread_number(); k++){
-		Core_finegrained_context(finegrained,k);
+		Core_finegrained_context(finegrained+k,k);
 	    printf("\nRegister file thread id %d:\n",k);
 	    for (int i = 0; i < REGS; ++i)
 	        printf("\tR%d = 0x%X", i, finegrained[k].reg[i]);
