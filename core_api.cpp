dba6b1a33d34e5677b5a73badc1db133fc1c5e4e
Sapir Malka <itssapir@gmail.com>
2019-06-15 13:26:57 +0300

    CPU object basis complete

--- core_api.cpp
+++ core_api.cpp
@@ -5,27 +5,31 @@
 #include <stdio.h>
 #include <vector>
 #include <list>
+#include <assert.h>
 
 using namespace std;
 
 typedef enum MODE {BLOCKED, FINEGRAIN} MODE;
 
+CPU blockedcpu(BLOCKED), finegraincpu(FINEGRAIN);
+
 class CPU
 {
 public:
-    CPU(unsigned numThreads, MODE mode);
+    CPU(MODE mode) : _mode(mode), _clock(0) { _switchPenalty = (mode == FINEGRAIN)? 0:Get_switch_cycles(); }
+    simReset(unsigned numThreads);
     void run(); // TODO: should we return anything?
     double getCPI();
     void getThreadContext(tcontext* bcontext, int threadid);
-    
+
 private:
     unsigned _numThreads;
-    unsigned _clock;
     MODE _mode;
+    unsigned _clock;
     unsigned _switchPenalty;
     vector<Thread> _threads;
     list<unsigned> _liveThreads;
-    
+
 };
 
 class Thread
@@ -47,28 +51,80 @@ private:
 
 };
 
+
+
+
+void CPU::run()
+{
+    while( !_liveThreads.empty() )
+    {
+        unsigned curThread = _liveThreads.front();
+        _liveThreads.pop_front(); // after iteration we will push it to the back if not done
+        unsigned clocksRan = _threads[curThread].run(_clock); // run the thread until it goes to idle or 1 clock if we are in finegrain
+        assert(clocksRan > 0);
+        _clock += _switchPenalty + clocksRan;
+        if(_threads[curThread].done == false)
+            _liveThreads.push_back(curThread);
+    }
+}
+
+void CPU::simReset(unsigned numThreads)
+{
+    // clear old simulation data if it existed
+    _threads.clear();
+    _liveThreads.clear();
+    _numThreads = numThreads;
+    _clock = 0;
+    // create new thread objects
+    for (int i = 0; i < numThreads; ++i)
+    {
+        _threads.push_back(Thread(_mode, i));
+        _liveThreads.push_back(i);
+    }
+}
+
+double CPU::getCPI()
+{
+    unsigned totalInsts = 0;
+    for(int i=0; i < _numThreads; ++i)
+        totalInsts += _threads[i].getTotalInstructions();
+    return ( (double)_clock ) / totalInsts;
+}
+
+void CPU::getThreadContext(tcontext* bcontext, int threadid)
+{
+    _threads[threadid].copyContext(bcontext);
+}
+
 Status Core_blocked_Multithreading(){
-	return Success;
+    // reset the blocked simulation and rerun it
+    blockedcpu.simReset(Get_thread_number());
+    blockedcpu.run();
+    return Success;
 }
 
 
 Status Core_fineGrained_Multithreading(){
-
-	return Success;
+    // reset the finegrain simulation and rerun it
+    finegraincpu.simReset(Get_thread_number());
+    finegraincpu.run();
+    return Success;
 }
 
 
 double Core_finegrained_CPI(){
-	return 0;
+    return finegraincpu.get_cpi();
 }
 double Core_blocked_CPI(){
-	return 0;
+    return blockedcpu.get_cpi();
 }
 
 Status Core_blocked_context(tcontext* bcontext,int threadid){
-	return Success;
+    blockedcpu.getThreadContext(bcontext, threadid);
+    return Success;
 }
 
 Status Core_finegrained_context(tcontext* finegrained_context,int threadid){
-	return Success;
+    finegraincpu.getThreadContext(bcontext, threadid);
+    return Success;
 }
